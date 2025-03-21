2f4d1d9d680dc859c7f7931c321da5c01a0756c7
freetimezp <kraftukrnet@ukr.net>
2025-03-21 21:50:35 +0200

    03. usefull-things-085-award-landing. GSAP reveal animation.

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+
+const tl = gsap.timeline();
+
+function revealSite() {
+    tl.to(".pre-loader", 1, {
+        opacity: 0,
+        display: "none",
+        ease: "power2.inOut"
+    });
+
+    tl.to(".header-row", 0.8, {
+        top: "0",
+        ease: "power4.inOut",
+        stagger: {
+            amount: 0.2
+        }
+    }, "-=1.2");
+
+    tl.from(".navbar > *, .footer", 2, {
+        y: 40,
+        oapcity: 0,
+        ease: "power4.inOut",
+        stagger: {
+            amount: 0.2
+        }
+    }, "-=1");
+
+    tl.to(".header > h1", 2, {
+        top: 0,
+        ease: "power3.inOut",
+        stagger: {
+            amount: 0.3
+        }
+    }).to(".pre-loader-btn", 0.3, {
+        oapcity: 1,
+        delay: 2
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
