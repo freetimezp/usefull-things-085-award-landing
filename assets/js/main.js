dd5dbbce1e9ed4cc1c4d3f39e3d9361cbf83d5c0
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-06-09 18:01:57 +0300

    03. usefull-things-085-award-landing. fix.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,47 @@
-
 const tl = gsap.timeline();
 
 function revealSite() {
     tl.to(".pre-loader", 1, {
         opacity: 0,
         display: "none",
-        ease: "power2.inOut"
+        ease: "power2.inOut",
     });
 
-    tl.to(".header-row", 0.8, {
-        top: "0",
-        ease: "power4.inOut",
-        stagger: {
-            amount: 0.2
-        }
-    }, "-=1.2");
+    tl.to(
+        ".header-row",
+        0.8,
+        {
+            top: "0",
+            ease: "power4.inOut",
+            stagger: {
+                amount: 0.2,
+            },
+        },
+        "-=1.2"
+    );
 
-    tl.from(".navbar > *, .footer", 2, {
-        y: 40,
-        oapcity: 0,
-        ease: "power4.inOut",
-        stagger: {
-            amount: 0.2
-        }
-    }, "-=1");
+    tl.from(
+        ".navbar > *, .footer",
+        2,
+        {
+            y: 40,
+            opacity: 0,
+            ease: "power4.inOut",
+            stagger: {
+                amount: 0.2,
+            },
+        },
+        "-=1"
+    );
 
     tl.to(".header > h1", 2, {
         top: 0,
         ease: "power3.inOut",
         stagger: {
-            amount: 0.3
-        }
+            amount: 0.3,
+        },
     }).to(".pre-loader-btn", 0.3, {
-        oapcity: 1,
-        delay: 2
+        opacity: 1,
+        delay: 2,
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
